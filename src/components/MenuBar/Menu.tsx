17458c7d94b16346c5354972117fce670876d665
Alex Arena <im@alexarena.com>
2020-04-27 12:45:02 -0700

    removed UI chrome

--- src/components/MenuBar/Menu.tsx
+++ src/components/MenuBar/Menu.tsx
@@ -1,8 +1,5 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import AboutDialog from './AboutDialog/AboutDialog';
-import IconButton from '@material-ui/core/IconButton';
-import MenuContainer from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 
 import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
 
@@ -10,9 +7,6 @@ export default function Menu() {
   const { room, localTracks } = useVideoContext();
 
   const [aboutOpen, setAboutOpen] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const anchorRef = useRef<HTMLDivElement>(null);
 
   const handleSignOut = useCallback(() => {
     room.disconnect?.();
@@ -20,27 +14,10 @@ export default function Menu() {
   }, [room.disconnect, localTracks]);
 
   return (
-    <div ref={anchorRef}>
-      <button onClick={() => setMenuOpen(state => !state)}>Toggle Menu</button>
-      <IconButton
-        color="inherit"
-        onClick={() => setMenuOpen(state => !state)}
-      ></IconButton>
-      <MenuContainer
-        open={menuOpen}
-        onClose={() => setMenuOpen(state => !state)}
-        anchorEl={anchorRef.current}
-      >
-        <MenuItem onClick={() => setAboutOpen(true)}>About</MenuItem>
-        <MenuItem onClick={handleSignOut}>Logout</MenuItem>
-      </MenuContainer>
-      <AboutDialog
-        open={aboutOpen}
-        onClose={() => {
-          setAboutOpen(false);
-          setMenuOpen(false);
-        }}
-      />
+    <div>
+      <button onClick={() => setAboutOpen(true)}>About</button>
+      <button onClick={handleSignOut}>Logout</button>
+      <AboutDialog open={aboutOpen} onClose={() => setAboutOpen(false)} />
     </div>
   );
 }
