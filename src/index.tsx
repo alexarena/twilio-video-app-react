1f461d9c2515e1aee1b17aaed70d46d1a3fa67a3
Alex Arena <im@alexarena.com>
2020-04-27 11:04:28 -0700

    rm private route

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,10 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 
 import App from './App';
 import AppStateProvider, { useAppState } from './state';
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ConnectOptions } from 'twilio-video';
 import ErrorDialog from './components/ErrorDialog/ErrorDialog';
 import { isMobile } from './utils';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import theme from './theme';
 import './types';
 import { VideoProvider } from './components/VideoProvider';
@@ -74,18 +68,13 @@ ReactDOM.render(
     <Router>
       <AppStateProvider>
         <Switch>
-          <PrivateRoute exact path="/">
-            <VideoApp />
-          </PrivateRoute>
-          <PrivateRoute path="/room/:URLRoomName">
+          <Route path="/room/:URLRoomName">
             <ClassDetailsProvider>
               <VideoApp />
             </ClassDetailsProvider>
-          </PrivateRoute>
-          <Redirect to="/" />
+          </Route>
         </Switch>
       </AppStateProvider>
-      >
     </Router>
   </MuiThemeProvider>,
   document.getElementById('root')
