39c0ea32a85fc9f2e88f5aab65c7d65d28db0fa0
Alex Arena <im@alexarena.com>
2020-04-27 13:27:35 -0700

    using localaudiolevelindicator

--- src/Room.tsx
+++ src/Room.tsx
@@ -14,6 +14,7 @@ import useHeight from './hooks/useHeight/useHeight';
 import useRoomState from './hooks/useRoomState/useRoomState';
 import useClassDetails from './ClassDetailsContext';
 import ToggleFullscreenButton from './components/ToggleFullScreenButton';
+import LocalAudioLevelIndicator from './components/LocalAudioLevelIndicator';
 
 const Container = styled('div')({
   display: 'grid',
@@ -95,6 +96,7 @@ export default function App() {
         <LogoutButton />
         <JoinButton />
         <ToggleFullscreenButton />
+        <LocalAudioLevelIndicator />
         <span>
           <strong>Room state:</strong>
           {roomState}
